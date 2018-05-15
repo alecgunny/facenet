8c8104f6bd63eac3877703a8fda2b0c3219eb179
Alec Gunny <agunny@nvidia.com>
2018-05-15 12:34:08 -0700

    modularized inference function construction

--- src/align/detect_face.py
+++ src/align/detect_face.py
@@ -307,46 +307,35 @@ class ONet(Network):
         (self.feed('prelu5') #pylint: disable=no-value-for-parameter
              .fc(10, relu=False, name='conv6-3'))
 
+
+def get_inference_functions(
+        network,
+        input_shape,
+        output_names,
+        weights,
+        gpu_options,
+        use_trt=False):
+    graph = tf.Graph()
+    with graph.as_default():
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+        with sess.as_default():
+            data = tf.placeholder(tf.float32, input_shape, 'input')
+            net = network({'data': data})
+            net.load(os.path.join(model_path, weights), sess)
+    return net.build_inference_fcn(
+        sess,
+        graph,
+        'input',
+        ouptut_names,
+        use_trt=use_trt)
+
 def create_mtcnn(gpu_options, model_path, use_trt=False):
     if not model_path:
         model_path,_ = os.path.split(os.path.realpath(__file__))
 
-    with tf.variable_scope('pnet'):
-        graph = tf.Graph()
-        with graph.as_default():
-            sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
-            with sess.as_default():
-                data = tf.placeholder(tf.float32, (None,None,None,3), 'input')
-                pnet = PNet({'data':data})
-                pnet.load(os.path.join(model_path, 'det1.npy'), sess)
-        pnet_fun = pnet.build_inference_fcn(
-            sess,
-            graph,
-            'input',
-            ['conv4-2/BiasAdd',
-            'prob1:0'],
-            use_trt=use_trt)
-
-    with tf.variable_scope('rnet'):
-        graph = tf.Graph()
-        with graph.as_default():
-            sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
-            with sess.as_default():
-                data = tf.placeholder(tf.float32, (None,24,24,3), 'input')
-                rnet = RNet({'data':data}, graph)
-                rnet.load(os.path.join(model_path, 'det2.npy'), sess)
-                rnet_fun = pnet.build_inference_fcn(sess, 'rnet/input:0', ['rnet/conv5-2/conv5-2:0', 'rnet/prob1:0'], use_trt=use_trt)
-
-    with tf.variable_scope('onet'):
-        graph = tf.Graph()
-        with graph.as_default():
-            sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
-            with sess.as_default():
-                data = tf.placeholder(tf.float32, (None,48,48,3), 'input')
-                onet = ONet({'data':data}, graph)
-                onet.load(os.path.join(model_path, 'det3.npy'), sess)
-                onet_fun = onet.build_inference_fcn(sess, 'onet/input:0', ['onet/conv6-2/conv6-2:0', 'onet/conv6-3/conv6-3:0', 'onet/prob1:0'], use_trt=use_trt)
-
+    pnet_fun = get_inference_function(PNet, (None,None,None,3), ['conv4-2/BiasAdd', 'prob1'], 'det1.npy', gpu_options, use_trt=use_trt)
+    rnet_fun = get_inference_function(RNet, (None,24,24,3), ['conv5-2/conv5-2', 'prob1'], 'det2.npy', gpu_options, use_trt=use_trt)
+    onet_fun = get_inference_function(ONet, (None,48,48,3), ['conv6-2/conv6-2', 'conv6-3/conv6-3', 'prob1'], 'det3.npy', gpu_options, use_trt=use_trt)
     return pnet_fun, rnet_fun, onet_fun
 
 def detect_face(img, minsize, pnet, rnet, onet, threshold, factor):
