4501f0672136a010fecdb14219bd3f14acd8cf93
Alec Gunny <agunny@nvidia.com>
2018-05-15 11:08:28 -0700

    added graph arg to Network calls

--- src/align/detect_face.py
+++ src/align/detect_face.py
@@ -315,7 +315,7 @@ def create_mtcnn(sess, model_path, use_trt=False):
         graph = tf.Graph()
         with graph.as_default():
             data = tf.placeholder(tf.float32, (None,None,None,3), 'input')
-            pnet = PNet({'data':data})
+            pnet = PNet({'data':data}, graph)
         pnet.load(os.path.join(model_path, 'det1.npy'), sess)
         pnet_fun = pnet.build_inference_fcn(sess, 'pnet/input:0', ['pnet/conv4-2/BiasAdd:0', 'pnet/prob1:0'], use_trt=use_trt)
 
@@ -323,7 +323,7 @@ def create_mtcnn(sess, model_path, use_trt=False):
         graph = tf.Graph()
         with graph.as_default():
             data = tf.placeholder(tf.float32, (None,24,24,3), 'input')
-            rnet = RNet({'data':data})
+            rnet = RNet({'data':data}, graph)
         rnet.load(os.path.join(model_path, 'det2.npy'), sess)
         rnet_fun = pnet.build_inference_fcn(sess, 'rnet/input:0', ['rnet/conv5-2/conv5-2:0', 'rnet/prob1:0'], use_trt=use_trt)
 
@@ -331,7 +331,7 @@ def create_mtcnn(sess, model_path, use_trt=False):
         graph = tf.Graph()
         with graph.as_default():
             data = tf.placeholder(tf.float32, (None,48,48,3), 'input')
-            onet = ONet({'data':data})
+            onet = ONet({'data':data}, graph)
         onet.load(os.path.join(model_path, 'det3.npy'), sess)
         onet_fun = onet.build_inference_fcn(sess, 'onet/input:0', ['onet/conv6-2/conv6-2:0', 'onet/conv6-3/conv6-3:0', 'onet/prob1:0'], use_trt=use_trt)
 
